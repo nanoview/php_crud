c4cb2bee1d49f7194ee60a188580aa6d3295c6e6
nanoview <arif.js@gmail.com>
2024-10-24 20:35:23 +0300

    New files

--- add_student.php
+++ add_student.php
@@ -40,20 +40,3 @@ if (isset($_SESSION['success_msg'])) {
 
 
 <?php include 'form.php'; ?>
-<div class="tbox">
-    <h2>Display success or error messages</h2>
-
-
-    <?php if (!empty($success_msg)): ?>
-        <!-- Display boxc when success message is present -->
-        <div class="boxc tbox">
-            <p style="color: green;"><?php echo htmlspecialchars($success_msg); ?></p>
-        </div>
-    <?php else: ?>
-        <!-- Display boxb when success message is absent -->
-        <div class="boxb tbox">
-            <p>No messages available.</p>
-        </div>
-    <?php endif; ?>
-
-</div>
