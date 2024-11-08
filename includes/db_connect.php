2e6cd95c96886035378e431584e67cba692c72ba
nanoview <arif.js@gmail.com>
2024-11-08 22:20:59 +0200

    app works

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,9 +1,8 @@
 <?php
-$serverName = "localhost";  // Replace with your server host (if not localhost)
-$username = "root";         // Replace with your DB username
+$serverName = "";  // Replace with your server host (if not localhost)
+$username = "";         // Replace with your DB username
 $password = "";             // Replace with your DB password
-$dbname = "students_db";
+$dbname = "";
 
 // Create connection
 $conn = new mysqli($serverName, $username, $password, $dbname);
-
